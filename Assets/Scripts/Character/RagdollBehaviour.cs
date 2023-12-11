3f682e4118b9e2370bd9f165d926f1093bceb082
madelalande <maxime.delalande@etudiant.univ-rennes1.fr>
2023-12-11 15:35:16 +0100

    Separate character with ragdoll and flying mode. Add character with ragdoll dying from DeathObstacles.

---
+++ Assets/Scripts/Character/RagdollBehaviour.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RagDollBehaviour : MonoBehaviour
+{
+    private Animator animator;
+    private BasicBehaviour basicBehaviour;
+    private MoveBehaviour moveBehaviour;
+    private AimBehaviourBasic aimBehaviourBasic;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        basicBehaviour = GetComponent<BasicBehaviour>();
+        moveBehaviour = GetComponent<MoveBehaviour>();
+        aimBehaviourBasic = GetComponent<AimBehaviourBasic>();
+
+        DisableRagdoll();
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag.Equals("ObstacleDeath"))
+        {
+            Debug.Log("Character death");
+            EnableRagdoll();
+        }
+    }
+
+    private void DisableRagdoll()
+    {
+        animator.enabled = true;
+        basicBehaviour.enabled = true;
+        moveBehaviour.enabled = true;
+        aimBehaviourBasic.enabled = true;
+    }
+
+    private void EnableRagdoll()
+    {
+        animator.enabled = false;
+        basicBehaviour.enabled = false;
+        moveBehaviour.enabled = false;
+        aimBehaviourBasic.enabled = false;
+    }
+}
