3e442e4252817562a6fa84a8c117e8372bb4a3c8
Dujardin Nolwenn <nolwenn.dujardin@etudiant.univ-rennes1.fr>
2023-12-12 09:18:36 +0100

    Modif obstacles
    PunchingWall ragdoll player
    
    Ajout panneau + text

--- Assets/Scripts/Obstacles/BounceBack.cs
+++ Assets/Scripts/Obstacles/BounceBack.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 public class BounceBack : MonoBehaviour
 {
     public float bounceForce = 100;
+    public float bounceDuration = 0.2f;
+    
     private void OnCollisionEnter(Collision other) 
     {
         if(other.gameObject.CompareTag("Player"))
         {
             Debug.Log("Bounce");
-            Rigidbody otherRB = other.gameObject.GetComponent<Rigidbody>();
-            otherRB.AddForce(other.GetContact(0).normal * bounceForce, ForceMode.Impulse);
+            StartCoroutine(ApplyBounceForce(other));
+        }
+    }
 
+    IEnumerator ApplyBounceForce(Collision other)
+    {
+        Rigidbody otherRB = other.gameObject.GetComponent<Rigidbody>();
+        Vector3 normal = other.GetContact(0).normal;
+
+        float timer = 0f;
+        while (timer < bounceDuration)
+        {
+            float force = Mathf.Lerp(0, bounceForce, timer / bounceDuration);
+            otherRB.AddForce(-normal * force);
+            timer += Time.deltaTime;
+            yield return null;
         }
     }
 
