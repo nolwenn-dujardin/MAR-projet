488c704a681f5acfd3ce1321ad3073214bcb2cc7
madelalande <maxime.delalande@etudiant.univ-rennes1.fr>
2024-01-27 01:33:39 +0100

    Add death cam. Remove water current.

--- Assets/Scripts/Environment/WaterRepulsion.cs
+++ Assets/Scripts/Environment/WaterRepulsion.cs
@@ -6,14 +6,13 @@ public class WaterRepulsion : MonoBehaviour
 {
     public float repulsionForce;
     public float deccelerationForce;
-    public float movingForce;
-
 
     private void OnTriggerStay(Collider other)
     {
         Rigidbody rb = other.attachedRigidbody;
         if (rb != null)
         {
+            // Deccélération à l'entrée dans l'eau
             if (rb.velocity.y < -1)
             {
                 rb.AddForce(Vector3.up * deccelerationForce, ForceMode.Acceleration);
@@ -24,9 +23,6 @@ public class WaterRepulsion : MonoBehaviour
             {
                 rb.AddForce(Vector3.up * repulsionForce, ForceMode.Acceleration);
             }
-
-            // Mouvement suivant le courrant
-            rb.AddForce(Vector3.forward * movingForce, ForceMode.VelocityChange);
         }
     }
 }
